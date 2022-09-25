cd8bd53fd1e61b848a0d74d592aa09faf5363578
Jens-Alrik Adrian <jensalrik.adrian@gmail.com>
2022-09-25 20:41:48 +0200

    Update plotter

--- plot_data.py
+++ plot_data.py
@@ -13,10 +13,14 @@ if __name__ == '__main__':
     simple_data = _read_csv(SIMPLE_CSV_FILENAME)
     alpha_beta_data = _read_csv(ALPHA_BETA_CSV_FILENAME)
 
-    # simple_data.plot(figsize=(12, 12/1.618))
-    alpha_beta_data.plot(
-        y=["noise_sample", "smoothed_sample"],
-        figsize=(12, 12/1.618)
-    )
+    simple_data.plot(figsize=(12, 12/1.618))
+
+    fig, axes = pyplot.subplots(nrows=2, sharex=True, figsize=(12, 12/1.618))
+    axes[0].plot(alpha_beta_data["noise_sample"], label="Noisy Data")
+    axes[0].plot(alpha_beta_data["smoothed_sample"], label="Smooth Estimate")
+    axes[1].plot(alpha_beta_data["velocity"])
+
+    axes[0].set_title("Positions")
+    axes[1].set_title("Velocity Estimate")
 
     pyplot.show()
