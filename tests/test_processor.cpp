0ab4a265160c38e85ca3cd52f8c0db636d3f3702
Jens-Alrik Adrian <jensalrik.adrian@gmail.com>
2022-09-26 23:53:30 +0200

    Towards DLL loading

--- tests/test_processor.cpp
+++ tests/test_processor.cpp
@@ -1,6 +1,22 @@
 #include <gtest/gtest.h>
+#include <dlfcn.h>
 
-TEST(HelloTest, BasicAssertions) {
-    EXPECT_STRNE("hello", "world");
-    EXPECT_EQ(7 * 6, 42);
+const std::string DLL_NAME = "/home/j/Dokumente/Git-Projects/CppPlayground/build/libprocessor.so";
+
+class RecursiveAveragingTest : public testing::Test {
+    protected:
+        void* dll_handle;
+
+        void SetUp() override {
+            dll_handle = dlopen((DLL_NAME).c_str(), RTLD_NOW);
+        }
+
+        void TearDown() override {
+            dlclose(dll_handle);
+        }
+};
+
+TEST_F(RecursiveAveragingTest, CanLoad) {
+    const char* error = dlerror();
+    EXPECT_EQ(*error, NULL);
 }
